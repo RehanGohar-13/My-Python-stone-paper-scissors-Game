0a1a983ec403ab2749cbe4b7f714a77dc7097905
Ateeq Rehman <221827389+RehanGohar-13@users.noreply.github.com>
2026-05-24 15:55:41 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,39 +3,46 @@ import random
 def get_choices():
     options = ["rock", "paper", "scissors"]
 
-    player_choice = input("Enter a choice (rock, paper, scissors): ").lower()
+    while True:
+        player_choice = input("Enter a choice (rock, paper, scissors): ").lower().strip()
+        if player_choice in options:
+            break
+        else:
+            print("Invalid choice. Try again.")
+    
+    computer_choice = random.choice(options)
+    
+    choices = {
+        "player": player_choice,
+        "computer": computer_choice
+    }
 
-    while player_choice not in options:
-        print("Invalid choice!")
-    player_choice = input("Enter rock, paper, or scissors: ").lower()
-
-    computer_chice = random.choice(options)
-
-    return {"player": player_choice, "computer": computer_chice}
+    return choices
 
 def check_win(player, computer):
-    print(f"You chose {player}, computer chose {computer}")
+    print(f"\nYou chose {player}, computer chose {computer}\n")
+
     if player == computer:
-        return("It's a tie!")
+        return "It's a tie!"
     
     elif player == "rock":
         if computer == "scissors":
-            return("Rock smashes scissors! You win!")
+            return "Rock smashes scissors! You win!"
         else:
-            return("Paper covers rock! You lose.")
-    
+            return "Paper covers rock! You lose."
+        
     elif player == "paper":
         if computer == "rock":
-            return("Paper covers rock! You win!")
+            return "Paper covers rock! You win!"
         else:
-            return("Scissors cuts paper! You lose.")
+            return "Scissors cuts paper! You lose."
         
     elif player == "scissors":
         if computer == "paper":
-            return("Scissors cuts paper! You win!")
+            return "Scissors cuts paper! You win!"
         else:
-            return("Rock smashes scissors! You lose.")
-
+            return "Rock smashes scissors! You lose."    
+               
 if __name__ == "__main__":
     choices = get_choices()
     result = check_win(choices["player"], choices["computer"])
